151afc12e0c6c88368b15a12d056c6e6225f7df8
FR0OST <148888273+FR0OST@users.noreply.github.com>
2024-08-22 18:27:36 +0000

    container config done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,17 @@
+const accountId = 14353 
+let accountEmail = "mytesting@gmail.com"
+var accountPassword = "12345"
+accountCity = "Chandigarh" //don't use this type 
+let accountState; // used when a value is not defined for a variable
+
+// accountId = 2
+
+accountEmail = "mytesting2@gmail.com"
+accountPassword = "23423423562"
+accountCity = "Hisar"
+
+// don't use var it will create problems in future code
+
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, , accountState])
---
+++ 01_basics/02_variable.js
@@ -0,0 +1,12 @@
+"use strict"; //treat all js code as newer version
+
+
+/*alert("Hello")
+
+alert( 3 + 3) //we can't use like this, it was old
+style as we are using node.js, not browser
+*/
+
+console.log( 3 + 3)
+
+console.log("Pritam");
