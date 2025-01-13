b7a92ac734fb29dd706b2375c6836ade7803af94
Pritam <148888273+FR0OST@users.noreply.github.com>
2025-01-13 14:16:55 +0000

    get

---
+++ .JS/script_part_5.js
@@ -0,0 +1,16 @@
+// Get element by TagName()
+/* 
+var a = document.getElementsByTagName('li');
+console.log(a);
+ */
+
+// Get element by ClassName
+
+var a = document.getElementsByClassName('hero');
+console.log(a);
+
+
+// getElement by Id
+
+var a = document.getElementById('zero');
+console.log(a);
